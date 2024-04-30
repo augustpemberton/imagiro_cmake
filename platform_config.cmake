5df88990c64ac8c37005b7cb345cdcc4bd57100e
August Pemberton <augustmpemberton@gmail.com>
2024-04-30 11:30:19 +0100

    asd

--- platform_config.cmake
+++ platform_config.cmake
@@ -9,10 +9,17 @@ if(APPLE)
     set (CMAKE_OSX_ARCHITECTURES "x86_64; arm64" CACHE STRING "Architectures" FORCE)
 endif()
 
+cmake_policy(SET CMP0069 NEW)
+set(CMAKE_POLICY_DEFAULT_CMP0069 NEW)
+
 if(CMAKE_BUILD_TYPE STREQUAL "Release" OR CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
     set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
 endif()
 
 if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-dev")
+endif()
+
+if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    add_compile_options($<$<CONFIG:Release>:-ffast-math>)
 endif()
